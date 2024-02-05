7491f13d62a0e9696c29a34eeccce4f265c94383
Andreas Kugel <andreas.kugel@web.de>
2024-02-05 10:20:35 +0100

    cosmetics. don't use base dir setting for mobile targets

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // use base to specify web base dir
+  // do not use when targeting android or ios
+  // base:"/ble/",
   plugins: [vue()],
 })
