e517ccab1a805b500e72217e1e0c3b04c8412ba8
Andreas Kugel <devel@akugel.de>
2024-02-23 13:27:20 +0100

    rover update

--- src/components/RoverCtl.vue
+++ src/components/RoverCtl.vue
@@ -1,116 +1,146 @@
 <template>
-    <div class="rover-ctl">
-        <div class="bgrid">
-            <va-button v-for="button in buttons" :key="button.id" @click="handleClick(button)">
-                {{ button.label }}
-            </va-button>
-        </div>
-        <div class="sgrid">
-            <va-slider v-for="slider in sliders" :key="slider.id" 
-            v-model="slider.value" :min="slider.min" :max="slider.max" 
-            @change="handleSliderChange(slider.id, slider.value)"></va-slider>
-        </div>
-        <div class="sgrid">
-            <va-checkbox v-for="checkbox in checkboxes" :key="checkbox.id" 
-            v-model="checkbox.checked" 
-            class="mb-6"
-            color="primary"
-            checked-icon="fa-check"
-            :label="checkbox.label"
-            @update:modelValue="handleCheckboxChange(checkbox.id, checkbox.checked)"
-            >
-        </va-checkbox>
-        </div>
+  <div class="rover-ctl">
+    <div class="bgrid">
+      <va-button
+        v-for="button in buttons"
+        :key="button.id"
+        @click="handleClick(button)"
+        :color="button.color"
+      >
+        {{ button.label }}
+      </va-button>
     </div>
+    <div class="sgrid">
+      <va-slider
+        v-for="slider in sliders"
+        :key="slider.id"
+        v-model="slider.value"
+        :min="slider.min"
+        :max="slider.max"
+        @change="handleSliderChange(slider.id, slider.value)"
+        :label="slider.label"
+        track-label-visible
+        class="sld"
+      >
+      </va-slider>
+    </div>
+    <div class="sgrid">
+      <va-checkbox
+        v-for="checkbox in checkboxes"
+        :key="checkbox.id"
+        v-model="checkbox.checked"
+        class="mb-6"
+        color="primary"
+        checked-icon="fa-check"
+        :label="checkbox.label"
+        @update:modelValue="handleCheckboxChange(checkbox.id, checkbox.checked)"
+      >
+      </va-checkbox>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, defineEmits } from 'vue';
-import { onMounted } from 'vue';
+import { ref, defineEmits } from "vue";
+import { onMounted } from "vue";
 
 onMounted(() => {
-    console.log("Mounted")
-    // Your mounted logic here
+  console.log("Mounted");
+  // Your mounted logic here
 });
 
-const emit = defineEmits(['button-click', 'slider-change', 'checkbox-change']);
+const emit = defineEmits(["button-click", "slider-change", "checkbox-change"]);
 
 const buttons = ref([
-    { id: 1, label: 'Button 1' },
-    { id: 2, label: 'Button 2' },
-    { id: 3, label: 'Button 3' },
-    { id: 4, label: 'Button 4' },
-    { id: 5, label: 'Button 5' },
-    { id: 6, label: 'Button 6' },
-    { id: 7, label: 'Button 7' },
-    { id: 8, label: 'Button 8' },
-    { id: 9, label: 'Button 9' },
+  { id: 1, label: "FWD Left", color: "primary" },
+  { id: 2, label: "FWD", color: "primary" },
+  { id: 3, label: "FWD Right", color: "primary" },
+  { id: 4, label: "Left", color: "primary" },
+  { id: 5, label: "Stop", color: "warning" },
+  { id: 6, label: "Right", color: "primary" },
+  { id: 7, label: "REV Left", color: "primary" },
+  { id: 8, label: "REV", color: "primary" },
+  { id: 9, label: "REV Right", color: "primary" },
 ]);
 
 const sliders = ref([
-    { id: 1, value: 5, min: 0, max:10 },
-    { id: 2, value: 0, min:-1,max:1 },
+  { id: 1, value: 5, min: 0, max: 10, label: "Speed" },
+  { id: 2, value: 0, min: -1, max: 1, label: "Rotation" },
 ]);
 
 const checkboxes = ref([
-    { id: 1, checked: false, label: 'Grip Left'},
-    { id: 2, checked: false, label: 'Grip Right'},
+  { id: 1, checked: false, label: "Grip Left" },
+  { id: 2, checked: false, label: "Grip Right" },
 ]);
 
 const handleClick = (button) => {
-    // Handle button click logic here
-    console.log(`Button ${button.id} clicked`);
-    emit('button-click', button.id);
+  // Handle button click logic here
+  console.log(`Button ${button.id} clicked`);
+  emit("button-click", button.id);
 };
 
 const handleSliderChange = (id, value) => {
-    // Handle slider change logic here
-    console.log(`Slider ${id} changed to ${value}`);
-    emit('slider-change', { id, value });
+  // Handle slider change logic here
+  console.log(`Slider ${id} changed to ${value}`);
+  emit("slider-change", { id, value });
 };
 
 const handleCheckboxChange = (id, checked) => {
-    // Handle checkbox change logic here
-    console.log(`Checkbox ${id} changed to ${checked}`);
-    emit('checkbox-change', { id, checked });
+  // Handle checkbox change logic here
+  console.log(`Checkbox ${id} changed to ${checked}`);
+  emit("checkbox-change", { id, checked });
 };
-
 </script>
 
 <style scoped>
 .rover-ctl {
-    max-width: 600px;
-    margin-top:1rem;
-    margin-bottom:1rem;
-    margin-left:auto;
-    margin-right:auto;
-   
+  max-width: 600px;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 1rem;
 }
 
 .bgrid {
-    margin-top:2rem;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 10px;
+  margin-top: 2rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
 }
 .sgrid {
-    margin-top:2rem;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 10px;
+  margin-top: 2rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
 }
 
 button {
-    width: 100%;
-    height: 100px;
+  width: 100%;
+  height: 100px;
 }
 
 .va-slider {
-    grid-column: span 2;
+  grid-column: span 2;
 }
 
 .va-checkbox {
-    text-align:center;
-    grid-column: span 1;
+  text-align: center;
+  grid-column: span 1;
+}
+</style>
+
+<style>
+.sld .va-input__label {
+  margin-right: 1rem;
+  width: 10%;
+  text-align: left;
+}
+
+@media (max-width: 500px) {
+  .sld .va-input__label {
+    width: 15%;
+    text-align: center;
+  }
 }
 </style>
