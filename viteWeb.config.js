7b853e910b030e05badce6bab8cde8060cd40bd2
Andreas Kugel <andreas.kugel@web.de>
2024-05-21 12:30:49 +0200

    web config

---
+++ viteWeb.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // use base to specify web base dir
+  // do not use when targeting android or ios
+  base:"/ble/",
+  plugins: [vue()],
+})
